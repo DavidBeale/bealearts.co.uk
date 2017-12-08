9b2f06af949f40c925ef1e3cfa5e3870c3e6d65d
David Beale <david.beale@bealearts.co.uk>
2017-12-08 15:39:56 +0000

    Prod build config

--- RELEASING.md
+++ RELEASING.md
@@ -2,6 +2,5 @@ Releasing
 =========
 
 1. Bump the version number using ```npm version``` and push
-2. Create a Release on GitHub
-3. A Travis CI build will be triggered
-4. The site will be published to GitHub pages
+2. A Travis CI build will be triggered
+3. The site will be published to GitHub pages
--- webpack.config.js
+++ webpack.config.js
@@ -78,13 +78,15 @@ module.exports = {
         }),
         new CopyWebpackPlugin([
             {from: 'CNAME'}
-        ])
+        ]),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+        })
     ], [
         new webpack.optimize.UglifyJsPlugin({
             minimize: true,
             sourceMap: true
         }),
-        new webpack.optimize.DedupePlugin(),
         new ExtractTextPlugin('site.css')
     ]),
     devtool: isProduction ? false : 'source-map',
